4f264686399b3636e32252f306b315afb5eadc9d
n.predein@profitbase.ru <n.predein@profitbase.ru>
2022-06-23 14:17:41 +0500

    refactor

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ function runOnKeys(func, ...codes) {
   let pressed = new Set();
   document.addEventListener('keydown', function(event) {
     pressed.add(event.code);
-  for (let code of codes) {
-    if (!pressed.has(code)) { return; }
-  }
+    for (let code of codes) {
+      if (!pressed.has(code)) { return; }
+    }
     pressed.clear();
     func();
   });
